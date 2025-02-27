e316edad86dddfbf4659d1ae0c2b24084f6d3b2d
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-02-27 15:16:08 +0200

    Bottom navigation created

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Styles from './styles/Styles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator()
+
+const LOCATIONS = 'Locations'
+const ADD_LOCATION = 'Add Location'
+const MAP = 'Map'
+const CAPITALS = 'Capitals'
+const icons = {
+  [LOCATIONS]: 'list',
+  [ADD_LOCATION]: 'add',
+  [MAP]: 'map',
+  [CAPITALS]: 'flag',
+}
 
 export default function App() {
   return (
-    <SafeAreaView>
-      
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={LOCATIONS} component={LocationsScreen}/>
+        <Tab.Screen name={ADD_LOCATION} component={AddLocationScreen}/>
+        <Tab.Screen name={MAP} component={MapScreen}/>
+        <Tab.Screen name={CAPITALS} component={CapitalsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
+const LocationsScreen = () => <View/>
+const AddLocationScreen = () => <View/>
+const MapScreen = () => <View/>
+const CapitalsScreen = () => <View/>
